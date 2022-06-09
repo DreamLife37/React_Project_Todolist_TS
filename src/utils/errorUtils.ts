efd95467668a07baad12f6102026ecdf707363d1
Andrey <fail4iks@gmail.com>
2022-06-09 22:09:39 +0300

    Step 15: Error, Loadings

---
+++ src/utils/errorUtils.ts
@@ -0,0 +1,14 @@
+import {AppActionsType, setAppErrorAC, setAppStatusAC} from "../app/app-reducer";
+import {Dispatch} from "redux";
+import {ResponseType} from "../api/todolists-api";
+
+
+export const handleNetworkError = (dispatch: Dispatch<AppActionsType>, message: string) => {
+    dispatch(setAppErrorAC(message))
+    dispatch(setAppStatusAC('failed'))
+}
+
+export const handleAppError = <T>(dispatch: Dispatch<AppActionsType>, data: ResponseType<T>) => {
+    dispatch(setAppErrorAC(data.messages[0]))
+    dispatch(setAppStatusAC('failed'))
+}
